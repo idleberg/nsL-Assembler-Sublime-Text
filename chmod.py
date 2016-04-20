9cc8cf11356cc58a26b651e4b709b2992e0ca567
Jan T. Sott <jan@idleberg.com>
2016-04-20 12:43:46 +0200

    use join()

--- chmod.py
+++ chmod.py
@@ -8,18 +8,16 @@ files = [
 ]
 
 def plugin_loaded():
+    from os.path import join
     from package_control import events
-    
-    # Get name of package folder
-    me = os.path.basename(os.path.dirname(os.path.realpath(__file__)))
 
-    if (events.install(me) or events.post_upgrade(me)) and os.name is 'posix' or 'mac':
+    if (events.install(package) or events.post_upgrade(package)) and os.name is 'posix' or 'mac':
         for file in files:
 
             # Concat full path
-            file_path = sublime.packages_path() + '/' + me + '/' + file
+            f = join(sublime.packages_path(), package + '/' + file)
 
             # Change permissions, if file exists
-            if os.path.isfile(file_path):
-                st = os.stat(file_path)
-                os.chmod(file_path, st.st_mode | stat.S_IEXEC)
+            if os.path.isfile(f):
+                st = os.stat(f)
+                os.chmod(f, st.st_mode | stat.S_IEXEC)
