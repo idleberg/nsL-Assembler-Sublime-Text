ee0867da9b970b70a808a7012f097e8ff365dcb5
Jan T. Sott <jan@idleberg.com>
2016-11-04 23:59:06 +0100

    update build system

---
+++ scripts/build-wine.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+command -v wine >/dev/null 2>&1 || { 
+    echo >&2 "Error: Wine not found"
+    exit 127
+}
+
+# Get Program Files path via Wine command prompt
+PROGRAMS_WIN=$(wine cmd /c 'echo %PROGRAMFILES%' 2>/dev/null)
+
+# Translate windows path to absolute unix path
+PROGRAMS_UNIX=$(winepath -u "${PROGRAMS_WIN}" 2>/dev/null)
+
+# Get NSIS path
+NSIS=$(printf %q "${PROGRAMS_UNIX%?}/NSIS")
+
+eval java -jar "$NSIS/NSL/nsL.jar" /nopause /nomake $@
---
+++ scripts/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Define location of nsL.jar
+NSL_JAR=
+
+if [ -z $NSL_JAR ]; then
+    echo "nsL.jar not defined in build script"
+elif [ -f $NSL_JAR ]; then
+    eval java -jar $NSL_JAR /nopause /nomake $@
+    exit 0
+else
+    echo "'$NSL_JAR' not found"
+fi
