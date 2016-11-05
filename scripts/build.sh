45f4b727d810c9779a685e6e3fb2605a7b3a9c65
Jan T. Sott <jan@idleberg.com>
2016-11-05 22:38:01 +0100

    fix backslash count

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,10 +4,10 @@
 NSL_JAR=
 
 if [ -z $NSL_JAR ]; then
-    echo "nsL.jar not defined in build script"
+    echo "Error: nsL.jar not defined in build script"
 elif [ -f $NSL_JAR ]; then
     eval java -jar $NSL_JAR /nopause /nomake $@
     exit 0
 else
-    echo "'$NSL_JAR' not found"
+    echo "Error: '$NSL_JAR' not found"
 fi
