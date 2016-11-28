49320a0527d56d069fc19bfd21e8dbca08c162f6
Jan T. Sott <jan@idleberg.com>
2016-11-28 21:38:15 +0100

    fix default nsL build flags

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,7 +6,7 @@ NSL_JAR=
 if [ -z $NSL_JAR ]; then
     echo "Error: nsL.jar not defined in build script"
 elif [ -f $NSL_JAR ]; then
-    eval java -jar $NSL_JAR /nomake $@
+    eval java -jar $NSL_JAR /nomake /nopause $@
     exit 0
 else
     echo "Error: '$NSL_JAR' not found"
